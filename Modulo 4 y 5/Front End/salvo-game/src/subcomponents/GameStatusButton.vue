9190a218c846d49ec93a0c984682468bd3e9cddc
Leo Sanchez <sanchez.leo88@gmail.com>
2019-10-29 00:37:08 -0300

    Avances en Front End. Comienzo de render de current games

---
+++ Modulo 4 y 5/Front End/salvo-game/src/subcomponents/GameStatusButton.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  props: {
+    game: Object,
+    user: Object
+  },
+
+  render(createElement) {
+    if (this.game.game_state == "over") {
+      return createElement("span", "Game Over");
+    } else {
+      return createElement("button", "things");
+    }
+  }
+};
+</script>
