d09d242826d36ae3c9ef45ef98daf8f333f60a37
Leo Sanchez <sanchez.leo88@gmail.com>
2019-12-07 20:58:53 -0300

    Agregado filtro en tabla games

--- Modulo 4 y 5/Front End/src/components/games/CurrentGamesTable.vue
+++ Modulo 4 y 5/Front End/src/components/games/CurrentGamesTable.vue
@@ -4,32 +4,24 @@
       <h1 class="headline font-italic font-weight-medium text-left">Active Games</h1>
       <v-simple-table height="400px" fixed-header>
         <thead>
-          <tr>
-            <td></td>
-          </tr>
           <th></th>
           <th class="title text-center">Player 1</th>
           <th class="title text-center">Player 2</th>
           <th class="title text-center">Created</th>
-          <th></th>
+          <th v-if="currentUser != null">
+            <v-switch
+              v-model="filter"
+              color="blue darken-3"
+              value="blue darken-3"
+              label="Hide Full Games"
+            ></v-switch>
+          </th>
         </thead>
 
         <tbody>
           <TableRow v-for="game in filteredGames" :game="game" :key="game.id" />
         </tbody>
       </v-simple-table>
-      <v-row justify="center" v-if="filter">
-        <v-col cols="auto">
-          <v-checkbox
-            v-model="filter"
-            color="blue darken-3"
-            value="blue darken-3"
-            label="Hide Full and Finished Games"
-            light
-            v-if="currentUser"
-          ></v-checkbox>
-        </v-col>
-      </v-row>
     </v-col>
   </v-row>
 </template>
